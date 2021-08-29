ab2f6a0ccbc4ef6f6edd7b9d4f59608b8f0a7420
cnridley <cnridley1@gmail.com>
2021-08-29 17:29:57 +0000

    added new images

--- products/views.py
+++ products/views.py
@@ -4,13 +4,11 @@ from products.models import nutritionGuides
 
 # Create your views here.
 
-#view to return the guides page.
-
 
 def guides(request):
 
-    gallery = Gallery.objects.all() #gets the images from th egallery model 
-    guides = nutritionGuides.objects.all() #gets all the info from the nutrition guides.
+    gallery = Gallery.objects.all()  # gets the images from the gallery model 
+    guides = nutritionGuides.objects.all()  # gets all the info from the nutrition guides.
 
     context = {
         'gallery': gallery,
